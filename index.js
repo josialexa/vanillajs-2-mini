a99ec3ad09e9cfd38db3df609d0254cfa490a9f1
Josi Reis-West <josialexa85@gmail.com>
2019-11-19 15:23:37 -0600

    Completed step 2

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-let addTodo = (e) => {
+const addTodo = (e) => {
     e.preventDefault();
     let newTodo = document.createElement('li');
     let newInput = document.getElementById('item').value;
 
     newTodo.innerText = newInput;
 
+    let todoButton = document.createElement('button');
+    todoButton.innerText = 'x';
+    todoButton.addEventListener('click', removeTodo);
+
+    newTodo.append(todoButton);
+
     document.querySelector('ul').appendChild(newTodo);
+}
+
+const removeTodo = (e) => {
+    e.target.parentNode.remove();
 }
 
 document.querySelector('form').addEventListener('submit', addTodo);
