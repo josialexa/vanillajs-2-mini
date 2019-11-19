ce278b60ac387de5d711653807a419c4dc8ea79f
Josi Reis-West <josialexa85@gmail.com>
2019-11-19 15:36:58 -0600

    Completed step 3 and Black Diamond

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const addTodo = (e) => {
     let newInput = document.getElementById('item').value;
 
     newTodo.innerText = newInput;
+    newTodo.addEventListener('click', completeTodo);
 
     let todoButton = document.createElement('button');
     todoButton.innerText = 'x';
@@ -15,7 +16,20 @@ const addTodo = (e) => {
 }
 
 const removeTodo = (e) => {
+    e.stopPropagation();
     e.target.parentNode.remove();
+}
+
+const completeTodo = (e) => {
+    if(e.target.getAttribute('aria-checked') != 'true') {
+        e.target.setAttribute('aria-checked', 'true');
+        document.getElementById('completed-message').style.visibility = 'visible';
+        window.setTimeout(() => {
+            document.getElementById('completed-message').style.visibility = 'hidden';
+        }, 2000);
+    } else {
+        e.target.setAttribute('aria-checked', 'false');
+    }
 }
 
 document.querySelector('form').addEventListener('submit', addTodo);
